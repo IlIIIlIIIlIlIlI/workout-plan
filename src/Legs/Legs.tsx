ce1f9c76f8c7516e8ef79dfa9f20f403f56548db
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-05-29 20:08:18 +0530

    new changes

--- src/Legs/Legs.tsx
+++ src/Legs/Legs.tsx
@@ -9,6 +9,31 @@ function Legs() {
         <li>Warm-Up</li>
         <li>50 Push-ups</li>
       </ol>
+
+      <ol>
+        <li>Barbell Squats - 14 || 15 x 3</li>
+        <li>Standing Lunges - 14 || 16 x 2</li>
+        <li>Walking Lunges - 14 || 16 x 1</li>
+      </ol>
+
+      <ol>
+        <li>Goblet Squats - 14 || 15 x 3</li>
+        <li>Sumo Squats - 14 || 20 x 3</li>
+      </ol>
+
+      <ol>
+        <li>Seated Calf Resist - 14 || 20 x 2</li>
+        <li>Standing Calf Resist - 14 || 20 x 2</li>
+      </ol>
+
+      <ol>
+        <li>Leg Extension - 14 || 16 x 3</li>
+        <li>Leg Curl - 14 || 16 x 3</li>
+      </ol>
+
+      <ol>
+        <li>Stretching</li>
+      </ol>
     </div>
   );
 }
