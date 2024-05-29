19c807c8b8a4b95659a452b7e9067bc48cdc1e58
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-05-29 20:35:36 +0530

    new changes

--- src/AbsAndCardio/AbsAndCardio.tsx
+++ src/AbsAndCardio/AbsAndCardio.tsx
@@ -4,7 +4,7 @@ import './AbsAndCardio.css';
 function AbsAndCardio() {
   return (
     <div className='main'>
-      <div className='date1'>Since 1st January 2024</div>
+      <div className='date1'>Since 1st April 2024</div>
       <div className='date'>Abs And Cardio</div>
       <ol>
         <li>Warm-Up</li>
@@ -13,8 +13,12 @@ function AbsAndCardio() {
 
       <ol>
         <li>Tredmill - 15 minutes || 3km</li>
-        <li>Elliptical - 15 minutes</li>
-        <li>Cycle - 15 minutes</li>
+        <li>
+          Elliptical - <del>15 minutes</del> - 7.5 minutes
+        </li>
+        <li>
+          Cycle - <del>15 minutes</del> - 7.5 minutes
+        </li>
       </ol>
 
       <ol>
