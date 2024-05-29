1b61c8fe05fcb5db004bce53671ebc8de5a3d601
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-05-29 20:23:14 +0530

    new changes

--- src/BackAndBiceps/BackAndBiceps.tsx
+++ src/BackAndBiceps/BackAndBiceps.tsx
@@ -10,6 +10,23 @@ function BackAndBiceps() {
       </ol>
 
       <ol>
+        <li>Bent OVer Row - 12 || 16 x 3</li>
+        <li>Broad Grip Lat Pull Down - 12 || 16 x 3</li>
+        <li>Narrow Grip Lat Pull Down - 12 || 16 x 3</li>
+        <li>Seated Row - 12 || 16 x 3</li>
+      </ol>
+
+      <ol>
+        <li>Shrugs - 12 || 16 x 3</li>
+      </ol>
+
+      <ol>
+        <li>Bicep Curl - 12 || 16 x 3</li>
+        <li>Hammer Curl - 12 || 16 x 3</li>
+        <li>Preacher Curl - 12 || 16 x 3</li>
+      </ol>
+
+      <ol>
         <li>ChinUps - 12 x 2</li>
         <li>Broad Grip Pullups - 12 x 2</li>
         <li>Stretching</li>
