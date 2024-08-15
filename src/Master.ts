1fdb994b028f3255d91e4986b04678d1e1ce4f0a
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2024-08-15 17:25:43 +0530

    new changes

--- src/Master.ts
+++ src/Master.ts
@@ -1,6 +1,11 @@
+export type Range = {
+  min: number;
+  max?: number;
+};
+
 export type Exercise = {
   exerciseName: string;
-  repeatition: number;
+  repeatition: Range;
   set: number;
   isActive?: boolean;
 };
@@ -30,42 +35,42 @@ const currentWarmUp: Variation[] = [
   {
     currentVariation: {
       exerciseName: 'Neck Rotation (Clockwise and Anti-clockwise each)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Shoulder Rotation (Clockwise and Anti-clockwise each)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Arms Rotation (Clockwise and Anti-clockwise each)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Hip circles (Clockwise and Anti-clockwise each)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Leg Swings',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Toe Rotation',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
@@ -75,14 +80,14 @@ export const currentStretching: Variation[] = [
   {
     currentVariation: {
       exerciseName: 'Finger Stretchs',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Finger Stretchs Reverse',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
@@ -90,42 +95,42 @@ export const currentStretching: Variation[] = [
     currentVariation: {
       exerciseName:
         'Arms on chest Stretch with another hand vartical to it (+ sign)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Triceps Stretch',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Arms on opposite wall (Reverse)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'One leg complete weight (vertical V)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Quadceps stretch (One leg on one glut by hand)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Quadceps stretch on bench',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
@@ -133,28 +138,28 @@ export const currentStretching: Variation[] = [
     currentVariation: {
       exerciseName:
         'Knee and Glut stretch on bench (Touching toes to the head)',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Calf strench',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'Cobra Pose',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
   {
     currentVariation: {
       exerciseName: 'D Pose',
-      repeatition: 20,
+      repeatition: { min: 20 },
       set: 1,
     },
   },
@@ -168,14 +173,14 @@ export const absAndCardio: Workout = {
     {
       currentVariation: {
         exerciseName: 'Push ups',
-        repeatition: 20,
+        repeatition: { min: 15, max: 20 },
         set: 1,
       },
 
       olderVariation: [
         {
           exerciseName: 'Push ups',
-          repeatition: 20,
+          repeatition: { min: 15, max: 20 },
           set: 3,
         },
       ],
@@ -189,14 +194,14 @@ export const absAndCardio: Workout = {
         {
           currentVariation: {
             exerciseName: 'Tread Mill (3km) (1km elevated at 3.0) (2km flat)',
-            repeatition: 15,
+            repeatition: { min: 15 },
             set: 1,
           },
         },
         {
           currentVariation: {
             exerciseName: 'Eliptical (15 minutes)',
-            repeatition: 15,
+            repeatition: { min: 15 },
             set: 1,
             isActive: false,
           },
@@ -204,14 +209,104 @@ export const absAndCardio: Workout = {
         {
           currentVariation: {
             exerciseName: 'Cycling (3.45 minutes)',
-            repeatition: 15,
+            repeatition: { min: 3, max: 4 },
             set: 1,
           },
           olderVariation: [
             {
               exerciseName: 'Cycling (15 minutes)',
-              repeatition: 15,
+              repeatition: { min: 15 },
               set: 1,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      name: 'Abs',
+      variations: [
+        {
+          currentVariation: {
+            exerciseName: 'Machine Crunches',
+            repeatition: {
+              min: 14,
+              max: 20,
+            },
+            set: 2,
+          },
+        },
+
+        {
+          currentVariation: {
+            exerciseName: 'Machine Oblique Crunches',
+            repeatition: {
+              min: 14,
+              max: 20,
+            },
+            set: 2,
+          },
+        },
+
+        {
+          currentVariation: {
+            exerciseName: 'Leg Raises',
+            repeatition: {
+              min: 14,
+              max: 20,
+            },
+            set: 2,
+          },
+        },
+
+        {
+          currentVariation: {
+            exerciseName: 'Mat Cycling',
+            repeatition: {
+              min: 50,
+            },
+            set: 1,
+          },
+        },
+
+        {
+          currentVariation: {
+            exerciseName: 'Weighted Decline Bench Crunches with 10kg',
+            repeatition: {
+              min: 14,
+              max: 20,
+            },
+            set: 2,
+          },
+          olderVariation: [
+            {
+              exerciseName: 'Mat Crunches',
+              repeatition: {
+                min: 20,
+                max: 20,
+              },
+              set: 2,
+            },
+          ],
+        },
+
+        {
+          currentVariation: {
+            exerciseName: 'Decline Bench Obliques',
+            repeatition: {
+              min: 14,
+              max: 20,
+            },
+            set: 2,
+          },
+          olderVariation: [
+            {
+              exerciseName: 'Mat Obliques Crunches',
+              repeatition: {
+                min: 20,
+                max: 20,
+              },
+              set: 2,
             },
           ],
         },
@@ -222,8 +317,10 @@ export const absAndCardio: Workout = {
   postWorkout: [
     {
       currentVariation: {
-        exerciseName: 'Passive Hanging (Back Stretch)',
-        repeatition: 20,
+        exerciseName: 'Active Hanging (Back Stretch)',
+        repeatition: {
+          min: 1,
+        },
         set: 1,
       },
     },
